41e00fd149273f6cd56b9921442838b8cec119be
DhivyaEaswaran <dhivyamathi@gmail.com>
2023-10-11 14:55:40 +0530

    updated

--- useref-hook/src/focusInput.js
+++ useref-hook/src/focusInput.js
@@ -1,14 +1,21 @@
-import React, {useRef,useState} from "react";
+import React, {useRef,useState,useEffect} from "react";
 
 function FocusInput() {
 const [name,setName]=useState("");    
 const inputRef=useRef();
-    
+const count=useRef(1);
+
+useEffect(()=>{
+count.current=count.current+1;
+});
+
   return (
     <div>
+    Name:
       <input ref={inputRef} type="text" value={name} onChange={e=>setName(e.target.value)}/>
       <div>My name is {name}</div>
-      <button onClick={()=>inputRef.current.focus()}>Click</button>
+      <button onClick={()=>{inputRef.current.focus()}}>Click to Focus</button>
+      <div>This has been rendered {count.current} times</div>
     </div>
   );
 }
